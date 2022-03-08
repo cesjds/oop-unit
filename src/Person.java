905685208cda711cf841a89cd4e345c51c615ba5
SkyQuiet12 <yoavamit@icloud.com>
2022-03-08 15:37:06 -0500

    PersonRunner sorts people

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,4 @@
-public class Person
+public class Person implements Comparable<Person>
 {
     // instance attributes
     // These are values that describe an instance of this class.
--- src/PersonRunner.java
+++ src/PersonRunner.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class PersonRunner
 {
@@ -8,13 +10,18 @@ public class PersonRunner
         ArrayList<Person> people = new ArrayList<Person>();
         Person p1 = new Person("2345", "Mary", "05/20/1938");
         Student s1 = new Student("1234", "Oliver", "11/18/2004" , 11, 4.0);
-        JOptionPane.showMessageDialog(null, p1);
-        JOptionPane.showMessageDialog(null, s1);
+        //JOptionPane.showMessageDialog(null, p1);
+        //JOptionPane.showMessageDialog(null, s1);
         people.add(p1);
         people.add(s1);
-        JOptionPane.showMessageDialog(null, people);
 
         people.add(new Student("99999", "Moshe", "04/07/2006", 10, 4.0));
         people.add(new Person("1111", "Izzy", "08/15/2022"));
+
+        JOptionPane.showMessageDialog(null, people);
+
+        Collections.sort(people);
+
+        JOptionPane.showMessageDialog(null, people);
     }
 }
