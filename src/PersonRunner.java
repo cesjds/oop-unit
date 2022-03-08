df3f871b3cbf3ba1bf5bdeec24d4f8efd68cc951
SkyQuiet12 <yoavamit@icloud.com>
2022-03-08 15:03:44 -0500

    PersonRunner class JOP prints ArrayList of student & person

--- src/PersonRunner.java
+++ src/PersonRunner.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class PersonRunner
 {
     public static void main(String[]args)
     {
-        Student p1 = new Student("1234", "Oliver", "11/18/2004" , 11, 4.0);
-        JOptionPane.showMessageDialog(null, p1.toString());
+        ArrayList<Person> people = new ArrayList<Person>();
+        Person p1 = new Person("2345", "Mary", "05/20/1938");
+        Student s1 = new Student("1234", "Oliver", "11/18/2004" , 11, 4.0);
+        JOptionPane.showMessageDialog(null, p1);
+        JOptionPane.showMessageDialog(null, s1);
+        people.add(p1);
+        people.add(s1);
+        JOptionPane.showMessageDialog(null, people);
     }
 }
