619612265bdb05e42427b87e01dca0ee89b667d0
Yoav Amit <yoavamit@icloud.com>
2022-03-20 21:40:14 -0400

    Add states.txt to data dir

--- src/StateDB.java
+++ src/StateDB.java
@@ -1,6 +1,36 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StateDB
 {
     public static void main(String[] args) {
+        ArrayList<State> states = new ArrayList<State>();
 
+        fillAray(State);
     }
+
+    public static String[][] fillAray(String states[][])
+    {
+        try {
+            Scanner inFile = new Scanner(new File("data/states.txt"));
+
+            while (inFile.hasNext())
+            {
+                for (int r = 0; r < 51; r++)
+                {
+                    for (int c = 0; c < 5; c++)
+                    {
+                        states[r][c] = inFile.nextLine();
+                    }
+                }
+            }
+            inFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return states;
+    }
+
 }
