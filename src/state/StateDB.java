7ef221fa14ef86d41278504065d62fe6235979ae
Yoav Amit <yoavamit@icloud.com>
2022-03-29 19:36:23 -0400

    Add search and modify instructions

--- src/state/StateDB.java
+++ src/state/StateDB.java
@@ -26,7 +26,41 @@ import java.util.Scanner;
  *
  * HAND IN:  Let me see your database print on your monitor.
  *
- *******************************************************************/
+ *
+ * ASSIGNMENT: Write a program that writes a new State database program
+ * 	using OOP and your State class.
+ *
+ * Your program should have a menu method that gives the user the following options:
+ * 	print, search, add, delete, exit
+ *
+ * print should print the entire database in the text area we used in your orignial program.
+ * search should ask for the name of a state and search for it. If it is found the
+ *   information for that state should be printed using the toString() from State.
+ * add should ask for the name of the state to be added. If that state is already in the
+ *   database you should print a message saying it is already there. Otherwise ask for
+ *   the rest of the information and add the state. You database should remain sorted
+ *   by state name.
+ * delete should ask for the name of the state to be deleted. If the state is in the database
+ *   it should be deleted.
+ * The user should be able to continue to run your program until they chooses to exit.
+ *
+ * NOTES:
+ * You cannot use 51 as the size of the ArrayList since it will be changing.
+ * You can create a dummy state using the name the user inputs, 4 empty Strings and 0.
+ * This will be useful in search() & add() although there are other ways to write these
+ * two methods.
+ * You MUST use this for delete() - most of the delete method can be written using
+ * methods from the ArrayList class.
+ * You shouldn't print the entire database after add and delete. Your program should
+ * go back to the main menu and the user can choose print from there.
+ * Your program should be running from your main method but all that should be there
+ * is declaring variables, a loop to keep the program going and method calls.
+ *
+ * HAND IN: after THOROUGHLY testing your program, copy and paste your code to
+ * Schoology and submit.
+ ********************************************************************/
+
+
 
 public class StateDB
 {
@@ -39,7 +73,8 @@ public class StateDB
         //state.State s1 = new state.State(sn,sc,sf,sb,sp);
     }
 
-    public static ArrayList<State> fillArray(ArrayList<State> states) {
+    public static ArrayList<State> fillArray(ArrayList<State> states)
+    {
         try {
             Scanner inFile = new Scanner(new File("data/states.txt"));
             while (inFile.hasNext()) {
@@ -55,7 +90,9 @@ public class StateDB
         }
         return states;
     }
-    public static void printArray(ArrayList<State> states) {
+
+    public static void printArray(ArrayList<State> states)
+    {
         JTextArea area = new JTextArea();
 
         //append column headings here
@@ -81,4 +118,6 @@ public class StateDB
         JScrollPane pane = new JScrollPane(area);
         JOptionPane.showMessageDialog(null, pane);
     }
+
+
 }
